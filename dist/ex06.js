9c05ad6c813db45f46ca750c2eb8d403459e442b
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2025-02-12 10:49:01 -0800

    add type

--- dist/ex06.js
+++ dist/ex06.js
@@ -1,9 +1,8 @@
+"use strict";
 // 6. Create a generic arrow function called `getValueFromObject` that accepts an object and a key,
 // and returns the value associated with that key from the object.
 // Use the object { js: "JavaScript", ts: "TypeScript" } and retrieve the value of the key "ts".
 // Log the result.
-
-const getValueFromObject = <T extends keyof U, U>(obj: U, key: T): U[T] => obj[key];
-
+const getValueFromObject = (obj, key) => obj[key];
 const languages = { js: "JavaScript", ts: "TypeScript" };
 console.log(getValueFromObject(languages, "ts")); // Expected output: "TypeScript"
